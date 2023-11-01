dae08c0b3d2933d7ba618075802cfc15746e98cd
Eduardo Andrade <eduardo.andrade.cedeno@udla.edu.ec>
2023-11-01 15:53:56 -0500

    Declaracion de Variables y modelo de funcion

--- main.c
+++ main.c
@@ -2,3 +2,15 @@
 #include<string.h>
 #include<math.h>
 
+int main (void){
+    int i, resultado, num;
+    printf("Ingrese un numero entero positivo\n");
+    scanf("%d", &num);
+
+    for (i= 0 ; i <= num; i++) 
+    {
+        resultado= ((pow(-1, i))/((2*i) + 1));
+        resultado+= resultado;
+    }
+    printf("%d", resultado);
+}
